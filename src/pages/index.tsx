7f265d950b0d176a9a58b23f8a52d9c094dc8b4e
banzak1 <leosantanabr@gmail.com>
2021-04-20 23:47:03 -0300

    Produção base do projeto com simulação dos json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+//SPA
+//SSR
+//SSG
+
+
+export default function Home(props) {
   return (
-    <h1>hello</h1>
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
